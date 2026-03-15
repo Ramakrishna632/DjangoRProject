ef7a44aae00229e03759efa72bcdfa38211baaa8
Ramakrishna632 <boddepalliramakrishna632@gmail.com>
2026-03-15 21:53:47 +0530

    update

--- R/asgi.py
+++ R/asgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
+
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'R.settings')
 
-application = get_wsgi_application()
+application = get_asgi_application()
